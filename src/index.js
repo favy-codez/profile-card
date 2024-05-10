47ce66286394d81dd20d7ce0b4c00ab238e8e0d1
favycodezz <ezeliorafavour81@gmail.com>
2024-05-09 21:02:38 -0700

    added components

--- src/index.js
+++ src/index.js
@@ -17,6 +17,31 @@ function App() {
   );
 }
 
+function Avatar(){
+    return(
+    <div>
+        <img src="./headshot.JPG" alt=""/>
+    </div>
+    )
+}
+
+function Intro(){
+    return (
+      <div>
+        <h1>Jonas Schmedtmann</h1>
+        <p>
+          Full-stack web developer and teacher at Udemy. When not coding or
+          preparing a course, I like to play board games, to cook (and eat), or
+          to just enjoy the Portuguese sun at the beach.
+        </p>
+      </div>
+    );
+}
+
+function SkillList() {
+  return <div className="skill-list"></div>;
+}
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
